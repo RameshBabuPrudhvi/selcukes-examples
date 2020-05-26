c7870145db1974dbed6ce4b0b30f6996ab312a22
Ramesh Babu Prudhvi <rprudhvi@outlook.com>
2020-05-26 14:26:41 +0530

    sample

--- src/test/java/com/techyworks/cucumber/steps/CucumberHooks.java
+++ src/test/java/com/techyworks/cucumber/steps/CucumberHooks.java
@@ -3,12 +3,23 @@ package com.techyworks.cucumber.steps;
 import io.cucumber.java.*;
 import io.github.selcukes.core.logging.Logger;
 import io.github.selcukes.core.logging.LoggerFactory;
+import io.github.selcukes.devtools.DevToolsService;
+import io.github.selcukes.devtools.core.Screenshot;
+import io.github.selcukes.devtools.services.ChromeDevToolsService;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class CucumberHooks {
     Logger logger = LoggerFactory.getLogger(this.getClass());
+    WebDriver driver;
 
     public CucumberHooks(Controller controller) {
+
         controller.setupController();
+        driver = controller.getDriver();
     }
 
     @Before
@@ -31,8 +42,12 @@ public class CucumberHooks {
     }
 
     @After
-    public void afterTest(Scenario scenario) {
+    public void afterTest(Scenario scenario) throws IOException {
         logger.info(() -> "After Scenario .." + scenario.getName());
+
+        ChromeDevToolsService devToolsService = DevToolsService.getDevToolsService(driver);
+        byte[] screenshot= Screenshot.captureFullPageAsBytes(devToolsService);
+        scenario.embed(screenshot, "image/png", "screenshot");
 
     }
 }
