2960237dd2fa624c1d1ee29efe96e2f74a226ed8
Ramesh Babu <rprudhvi@outlook.com>
2020-06-21 20:21:33 +0530

    Few Report changes

--- src/test/java/com/techyworks/cucumber/steps/Controller.java
+++ src/test/java/com/techyworks/cucumber/steps/Controller.java
@@ -2,20 +2,36 @@ package com.techyworks.cucumber.steps;
 
 import io.github.selcukes.wdb.WebDriverBinary;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Controller {
     private WebDriver driver;
+
     public WebDriver getDriver() {
         return driver;
     }
+
     public void setDriver(WebDriver driver) {
         this.driver = driver;
     }
+
     public void setupController() {
+
         WebDriverBinary.chromeDriver().setup();
-        this.driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        try {
+            this.driver = new RemoteWebDriver(
+                new URL("http://172.18.62.149:30001/wd/hub"),
+                options);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
+
     public void teardownController() {
         if (driver != null) {
             driver.quit();
--- src/test/java/com/techyworks/cucumber/steps/CucumberHooks.java
+++ src/test/java/com/techyworks/cucumber/steps/CucumberHooks.java
@@ -2,30 +2,30 @@ package com.techyworks.cucumber.steps;
 
 
 import io.cucumber.java.*;
+import io.github.selcukes.core.config.ConfigFactory;
 import io.github.selcukes.core.logging.Logger;
 import io.github.selcukes.core.logging.LoggerFactory;
 import io.github.selcukes.reports.screen.ScreenPlay;
 import io.github.selcukes.reports.screen.ScreenPlayBuilder;
 import org.openqa.selenium.WebDriver;
 
-import java.io.IOException;
-
 public class CucumberHooks {
-    Logger logger = LoggerFactory.getLogger(this.getClass());
+    Logger logger = LoggerFactory.getLogger(getClass());
     WebDriver driver;
     ScreenPlay screenPlay;
 
     public CucumberHooks(Controller controller) {
-
+        ConfigFactory.loadLoggerProperties();
         controller.setupController();
         driver = controller.getDriver();
     }
 
     @Before
     public void beforeTest(Scenario scenario) {
+
+
         screenPlay = ScreenPlayBuilder.getScreenPlay(driver, scenario);
         screenPlay.start();
-
 
         logger.info(() -> "Before Scenario .." + scenario.getName());
     }
@@ -41,15 +41,17 @@ public class CucumberHooks {
 
         logger.info(() -> "After Step");
         logger.info(() -> "Scenario Status:" + scenario.getStatus());
+        /*String allLogs = logRecordListener.getLogRecords()
+            .map(LogRecord::getMessage)
+            .collect(Collectors.joining("\n  --> ", "\n--ALL Logs-- \n\n  --> ", "\n\n--End Of Logs--"));
 
+        scenario.write(allLogs);*/
     }
 
     @After
-    public void afterTest(Scenario scenario) throws IOException {
+    public void afterTest(Scenario scenario) {
         logger.info(() -> "After Scenario .." + scenario.getName());
         screenPlay.attachScreenshot();
         screenPlay.attachVideo();
-        System.setProperty("selcukes.teams.hooksUrl","");
-        screenPlay.teamsNotification(scenario.getName());
     }
 }
