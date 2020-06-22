48fb9b5cc629eae8355df806a88671dd28545de7
Ramesh Babu <rprudhvi@outlook.com>
2020-06-22 16:35:13 +0530

    Core Reporting

--- src/test/java/com/techyworks/cucumber/runner/CucumberTestRunner.java
+++ src/test/java/com/techyworks/cucumber/runner/CucumberTestRunner.java
@@ -6,7 +6,9 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features",
     glue = {"com.techyworks.cucumber.steps"},
-    plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber-reports/cucumber.json"},
+    plugin = {"pretty", "html:target/cucumber-reports",
+      //  "io.github.selcukes.reports.cucumber.Selcukes",
+        "json:target/cucumber-reports/cucumber.json"},
     monochrome = true,
     strict = true,
     tags = {"@pdf"})
--- src/test/java/com/techyworks/cucumber/steps/Controller.java
+++ src/test/java/com/techyworks/cucumber/steps/Controller.java
@@ -2,6 +2,7 @@ package com.techyworks.cucumber.steps;
 
 import io.github.selcukes.wdb.WebDriverBinary;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -22,14 +23,15 @@ public class Controller {
     public void setupController() {
 
         WebDriverBinary.chromeDriver().setup();
-        ChromeOptions options = new ChromeOptions();
+        driver=new ChromeDriver();
+        /*ChromeOptions options = new ChromeOptions();
         try {
             this.driver = new RemoteWebDriver(
                 new URL("http://172.18.62.149:30001/wd/hub"),
                 options);
         } catch (MalformedURLException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     public void teardownController() {
