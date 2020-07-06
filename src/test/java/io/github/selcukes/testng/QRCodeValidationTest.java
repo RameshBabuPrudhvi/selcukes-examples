38e0c03053e8cef4dd553b737190560ed718c8e3
Ramesh Babu <rprudhvi@outlook.com>
2020-07-06 16:18:25 +0530

    Updated Test

--- src/test/java/io/github/selcukes/testng/QRCodeValidationTest.java
+++ src/test/java/io/github/selcukes/testng/QRCodeValidationTest.java
@@ -3,6 +3,8 @@ package io.github.selcukes.testng;
 import com.google.zxing.*;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
+import io.github.selcukes.core.logging.Logger;
+import io.github.selcukes.core.logging.LoggerFactory;
 import io.github.selcukes.wdb.WebDriverBinary;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,21 +14,21 @@ import org.testng.annotations.Test;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
 public class QRCodeValidationTest {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @Test
     public void testQRCode() throws Exception {
         WebDriverBinary.chromeDriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://cdn.qrstuff.com/");
         String qrCodeURL = driver.findElement(By.id("qrcode-preview")).getAttribute("src");
+        logger.info(() -> "QR Code Url :" + qrCodeURL);
         URL url = new URL(qrCodeURL);
-        try (BufferedInputStream in = new BufferedInputStream(url.openStream());
+      /*  try (BufferedInputStream in = new BufferedInputStream(url.openStream());
              FileOutputStream fileOutputStream = new FileOutputStream("Sample-QR.png")) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
@@ -36,20 +38,22 @@ public class QRCodeValidationTest {
         } catch (IOException ignored) {
 
         }
-        File file = new File("Sample-QR.png");
-        validateQRCode(file);
-        Assert.assertEquals(validateQRCode(file), "http://www.qrstuff.com");
+        File file = new File("Sample-QR.png");*/
+
+        String qrCodeText = validateQRCode(url);
+        logger.info(() -> "Actual QR Code Text :" + qrCodeText);
+        Assert.assertEquals(qrCodeText, "http://www.qrstuff.com");
     }
 
-    public String validateQRCode(File qrCodeImage) throws IOException {
-        BufferedImage bufferedImage = ImageIO.read(qrCodeImage);
+    public String validateQRCode(URL qrCode) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(qrCode);
         LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
         BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
         try {
             Result result = new MultiFormatReader().decode(bitmap);
             return result.getText();
         } catch (NotFoundException e) {
-            System.out.println("There is no QR code in the image");
+            logger.error(() -> "There is no QR code in the image");
             return null;
 
         }
